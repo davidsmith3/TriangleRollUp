263b33c8598898eef3e97aac507461cdea2bce40
davidsmith3 <david.smith3@outlook.com>
2014-09-27 20:43:09 -0400

    built basic test cases

---
+++ TriangleRollUp.Domain/Generator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleRollUp.Domain {
+    public class Generator {
+        public string BuildTriangle(List<int> nums) {
+            throw new NotImplementedException();
+        }
+    }
+}
