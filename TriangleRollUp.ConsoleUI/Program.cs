b15503ff58d1aa9b0fcce09e5c33185992c8360f
davidsmith3 <david.smith3@outlook.com>
2014-09-27 21:27:47 -0400

    passing basic tests

--- TriangleRollUp.ConsoleUI/Program.cs
+++ TriangleRollUp.ConsoleUI/Program.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TriangleRollUp.Domain;
 
 namespace TriangleRollUp.ConsoleUI {
     class Program {
         static void Main(string[] args) {
+            Generator g = new Generator();
+            List<int> nums = new List<int>() { 4, 7, 3, 6, 7 };
+
+            Console.Write(g.BuildTriangle(nums));
+            Console.ReadLine();
         }
     }
 }
--- TriangleRollUp.Domain/Generator.cs
+++ TriangleRollUp.Domain/Generator.cs
@@ -7,7 +7,43 @@ using System.Threading.Tasks;
 namespace TriangleRollUp.Domain {
     public class Generator {
         public string BuildTriangle(List<int> nums) {
-            throw new NotImplementedException();
+            if (nums.Count == 0) return String.Empty;
+            if (nums.Count == 1) return String.Format("{0}\n", nums[0]);
+
+            List<int> current = nums;
+            Stack<List<int>> triangle = new Stack<List<int>>();
+            triangle.Push(null);
+            do {
+                triangle.Push(current);
+                List<int> next = new List<int>();
+                int back = 0;
+                int front = 1;
+                while (front < current.Count) {
+                    next.Add(current[back] + current[front]);
+                    back++;
+                    front++;
+                }
+                current = next;
+            } while (current.Count > 1);
+            triangle.Push(current);
+            int tabs = nums.Count - 1;
+            StringBuilder sb = new StringBuilder();
+            List<int> row = triangle.Pop();
+            while (row != null) {
+                for (int i = 0; i < tabs; i++) {
+                    sb.Append("\t");
+                }
+                for (int i = 0; i < row.Count; i++) {
+                    sb.Append(row[i]);
+                    sb.Append("\t\t");
+                }
+                sb.Append("\n");
+                
+                tabs--;
+                row = triangle.Pop();
+            }
+
+            return sb.ToString();
         }
     }
 }
